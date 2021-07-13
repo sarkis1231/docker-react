30da4e3d51985a5ca4667d4758867dbc08ba776c
sarkis1231 <sakooghly@gmail.com>
2021-07-13 20:24:15 +0400

    changed the App file

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reactt
+          Learn React
         </a>
       </header>
     </div>
